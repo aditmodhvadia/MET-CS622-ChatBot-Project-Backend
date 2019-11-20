880b7b0570b7330fee14d77ba10b6835edda6dfc
Vidhey Oza <vidhey97@gmail.com>
2019-11-19 20:23:58 -0500

    - Initial Commit _ setup and running

---
+++ src/servlets/hi/HiWorldServlet.java
@@ -0,0 +1,16 @@
+package servlets.hi;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HiWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hi world servlet called");
+        resp.getOutputStream().print("Hi world");
+    }
+}
---
+++ src/servlets/startup/StartUpServlet.java
@@ -0,0 +1,14 @@
+package servlets.startup;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class StartUpServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("--------#####--------");
+        System.out.println("        Server started      ");
+        System.out.println("--------#####--------");
+//        Execute work from here
+    }
+}
