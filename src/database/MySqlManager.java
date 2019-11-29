9be677113ebe86a676d4fe6b44711efee4caf439
Adit Modhvadia <adit.modhvadia@gmail.com>
2019-11-28 19:46:28 -0500

    - make separate method for inserting into ActivFitSensorData

--- src/database/MySqlManager.java
+++ src/database/MySqlManager.java
@@ -1,6 +1,6 @@
 package database;
 
-import sensormodels.HeartRateSensorData;
+import sensormodels.ActivFitSensorData;
 
 import java.sql.*;
 
@@ -13,97 +13,82 @@ public class MySqlManager {
     private static final String ACTIVITY_TABLE = "ActivitySensorData";
     private static final String BATTERY_TABLE = "BatterySensorData";
     private static final String BLUETOOTH_TABLE = "BluetoothSensorData";
-    private static final String HART_RATE_TABLE = "HeartRateSensorData";
+    private static final String HEART_RATE_TABLE = "HeartRateSensorData";
     private static final String LIGHT_TABLE = "LightSensorData";
-    private static final String SCREEN_USAGE_TABLE="ScreenUsageSensorData";
+    private static final String SCREEN_USAGE_TABLE = "ScreenUsageSensorData";
 
     //  Database credentials
     static final String USER = "root";
     static final String PASS = "Superj35@";
+    private static Connection connection;
 
     public static void init() {
-        Connection connection = null;
+        connection = null;
         Statement stmt = null;
-        try{
+        try {
             //STEP 2: Register JDBC driver
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             //STEP 3: Open a connection
             System.out.println("Connecting to database...");
-           connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
 
             //STEP 4: Execute a query
             System.out.println("Creating database...");
             stmt = connection.createStatement();
 
             String sql = "CREATE TABLE " + ACTIVITY_TABLE +
-            "(timestamp TIME , " +
+                    "(timestamp TIME , " +
                     " time_stamp TIMESTAMP , " +
                     " sensor_data CHAR(5) , " +
                     " step_counts INTEGER, " +
                     " step_delta INTEGER, " +
                     " PRIMARY KEY ( timestamp))";
             // creating ActivFitSensorData table
-            String sql2 = "CREATE TABLE " +ACTIV_FIT_TABLE +
+            String sql2 = "CREATE TABLE " + ACTIV_FIT_TABLE +
                     "(timestamp TIME , " +
                     " start_time TIMESTAMP , " +
                     " end_time TIMESTAMP , " +
-                    " duration INTEGER, "+
+                    " duration INTEGER, " +
                     " activity VARCHAR (255)) ";
 
-                    //" PRIMARY KEY ( timestamp))";
+            //" PRIMARY KEY ( timestamp))";
             // creating BatterySensorData table
-            String sql3 = "CREATE TABLE " +BATTERY_TABLE +
+            String sql3 = "CREATE TABLE " + BATTERY_TABLE +
                     "(timestamp TIME , " +
                     " time_stamp TIMESTAMP , " +
                     " sensor_data CHAR (5), " +
                     " percent INTEGER , " +
                     " charging BIT ) ";
-                    //" PRIMARY KEY ( timestamp))";
+            //" PRIMARY KEY ( timestamp))";
 
             // creating BluetoothSensorData table
-            String sql4 = "CREATE TABLE " +BLUETOOTH_TABLE +
+            String sql4 = "CREATE TABLE " + BLUETOOTH_TABLE +
                     "(timestamp TIME , " +
                     " time_stamp TIME , " +
                     " sensor_data CHAR (5), " +
                     " state CHAR (225)) ";
 
             // creating HeartRateSensorData table
-            String sql5 = "CREATE TABLE " +HART_RATE_TABLE+
+            String sql5 = "CREATE TABLE " + HEART_RATE_TABLE +
                     "(timestamp TIME , " +
                     " sensor_data CHAR (5), " +
                     " bpm INTEGER)";
             // creating LightSensorData table
-            String sql6 = "CREATE TABLE " +LIGHT_TABLE+
+            String sql6 = "CREATE TABLE " + LIGHT_TABLE +
                     "(timestamp TIME , " +
                     " time_stamp TIME , " +
                     " sensor_data CHAR (5), " +
                     " lux INTEGER) ";
             // creating ScreenUsageSensorData
-            String sql7 = "CREATE TABLE " + SCREEN_USAGE_TABLE+
+            String sql7 = "CREATE TABLE " + SCREEN_USAGE_TABLE +
                     "(start_hour INTEGER , " +
-                    " end_hour INTEGER,"+
+                    " end_hour INTEGER," +
                     " start_timestamp TIME,  " +
-                    " end_timestamp TIME,  "+
+                    " end_timestamp TIME,  " +
                     " min_elapsed DOUBLE , " +
                     " min_start_hour DOUBLE , " +
                     " min_end_hour DOUBLE ) ";
-
-            // the mysql insert statement
-            String sql1 = " insert into ActivitySensorData (timestamp , time_stamp, sensor_data, step_counts,step_delta,)"
-                    + " values (?, ?, ?, ?, ?)";
-
-            // create the mysql insert preparedstatement
-            PreparedStatement preparedStmt = connection.prepareStatement(sql1);
-            preparedStmt.setTimestamp (1, "");
-            preparedStmt.setTimestamp (2, "");
-            preparedStmt.setString   (3, );
-            preparedStmt.setInt(4, );
-            preparedStmt.setInt    (5, );
-
-            // execute the preparedstatement
-            preparedStmt.execute();
-
 
             // executing statements to created SenorData database tables
             stmt.executeUpdate(sql);
@@ -114,26 +99,49 @@ public class MySqlManager {
             stmt.executeUpdate(sql6);
             stmt.executeUpdate(sql7);
             System.out.println("Created tables in given database...");
-        }catch(SQLException se){
+        } catch (SQLException se) {
             //Handle errors for JDBC
             se.printStackTrace();
-        }catch(Exception e){
+        } catch (Exception e) {
             //Handle errors for Class.forName
             e.printStackTrace();
-        }finally{
+        } finally {
             //finally block used to close resources
-            try{
-                if(stmt!=null)
+            try {
+                if (stmt != null)
                     stmt.close();
-            }catch(SQLException se2){
+            } catch (SQLException ignored) {
             }// nothing we can do
-            try{
-                if(connection!=null)
+            try {
+                if (connection != null)
                     connection.close();
-            }catch(SQLException se){
+            } catch (SQLException se) {
                 se.printStackTrace();
             }//end finally try
         }//end try
         System.out.println("Goodbye!");
     }//end main
+
+    public void insertIntoActivFitTable(ActivFitSensorData sensorData) {
+        // the mysql insert statement
+        String sql1 = " insert into " + ACTIV_FIT_TABLE + " (timestamp , time_stamp, sensor_data, step_counts,step_delta,)"
+                + " values (?, ?, ?, ?, ?)";
+
+        // create the mysql insert preparedstatement
+        PreparedStatement preparedStmt = null;
+        try {
+//            preparedStmt = connection.prepareStatement(sql1);
+//            preparedStmt.setTimestamp(1, "");
+//            preparedStmt.setTimestamp(2, "");
+//            preparedStmt.setString(3, );
+//            preparedStmt.setInt(4, );
+//            preparedStmt.setInt(5, );
+
+            // execute the preparedstatement
+            preparedStmt.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
